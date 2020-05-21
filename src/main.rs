ff253d7c066e90fe5553ad3028ba97ec45ccad1a
Zachary Thomas <zdcthomas@yahoo.com>
2020-05-20 18:23:38 -0600

    ACTUALLY REFACTORS STUFF WOOT

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
+#[macro_use]
+extern crate serde_derive;
 extern crate clap;
 extern crate config;
 extern crate dirs;
-
-#[macro_use]
-extern crate serde_derive;
-
 extern crate grep_cli;
 extern crate skim;
 extern crate tmux_interface;
@@ -15,73 +13,61 @@ mod app;
 mod select;
 mod tmux;
 
-use app::{
-    CommandType::{Local, Pull},
-    Config,
-};
+use app::CommandType::{Open, Pull, Select};
 use select::Selector;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use tmux::{Layout, WorkSpace};
 use url::Url;
 
-fn setup_workspace(config: Config, maybe_dir: Option<PathBuf>) {
-    let selected_dir: PathBuf;
-    if let Some(dir) = maybe_dir {
-        selected_dir = dir;
-    } else if let Some(dir) = config.selected_dir {
-        selected_dir = dir;
-    } else {
-        panic!("No dir was findable");
-    }
-
-    // TODO: can i get rid of this? Put this in a validator?
-    if !selected_dir.exists() {
-        panic!("dude, that's not a path")
-    }
-    let layout = Layout {
-        layout_string: config.layout,
-        window_count: config.number_of_panes,
-    };
-    let workspaces = WorkSpace {
-        commands: config.commands,
-        dir: String::from(selected_dir.to_str().unwrap()),
-        layout,
-        session_name: config.session_name,
-        window_name: path_to_window_name(&selected_dir).to_string(),
-    };
-    tmux::setup_workspace(workspaces);
-}
-
 fn main() {
     let command = app::build_app();
 
     match command {
-        Local(config) => {
-            if config.selected_dir.is_some() {
-                setup_workspace(config, None)
-            } else if let Some(dir) = Selector::new(&config.search_dir).select_dir() {
-                setup_workspace(config, Some(dir))
-            } else {
-                panic!()
+        Open(open_config) => open_selected_dir(open_config),
+        Select(select_config) => {
+            if let Some(dir) = Selector::new(&select_config.workspace.search_dir).select_dir() {
+                open_selected_dir(app::OpenArgs {
+                    selected_dir: dir,
+                    workspace: select_config.workspace,
+                })
             }
         }
-
         Pull(pull_config) => {
             let dir = clone_from(&pull_config);
-            setup_workspace(pull_config.open_config, Some(dir))
+            open_selected_dir(app::OpenArgs {
+                selected_dir: dir,
+                workspace: pull_config.workspace,
+            })
         }
     }
+}
+
+fn open_selected_dir(config: app::OpenArgs) {
+    if !config.selected_dir.exists() {
+        panic!("dude, that's not a path")
+    }
+    let layout = Layout {
+        layout_string: config.workspace.layout,
+        window_count: config.workspace.number_of_panes,
+    };
+    let workspaces = WorkSpace {
+        commands: config.workspace.commands,
+        dir: String::from(config.selected_dir.to_str().unwrap()),
+        layout,
+        session_name: config.workspace.session_name,
+        window_name: path_to_window_name(&config.selected_dir).to_string(),
+    };
+    tmux::setup_workspace(workspaces);
 }
 
 // TODO: -> Result<Output, Error>
 fn git_url_to_dir_name(url: &Url) -> String {
     let segments = url.path_segments().ok_or_else(|| "cannot be base").unwrap();
-    // TODO: use str.replace here
     segments.last().unwrap().replace(".git", "")
 }
 
-fn clone_from(config: &app::PullConfig) -> PathBuf {
+fn clone_from(config: &app::PullArgs) -> PathBuf {
     let dir_name = git_url_to_dir_name(&config.repo_url);
     let target = config.target_dir.join(dir_name);
     if !target.exists() {
