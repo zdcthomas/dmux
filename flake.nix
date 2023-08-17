54e071ff460df74bef31853e864e92f2db15110a
zdcthomas <zdcthomas@yahoo.com>
2023-08-16 23:55:17 -0400

    add overlay

--- flake.nix
+++ flake.nix
@@ -13,6 +13,12 @@
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
+        overlays.default = [
+          (final: prev: {
+            dmux = naersk-lib.buildPackage ./.;
+          })
+
+        ];
         devShell = with pkgs; mkShell {
           buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
