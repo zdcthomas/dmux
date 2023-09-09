eb9f15fbacda1ce08b272ce57c54747ea1889d49
zdcthomas <zdcthomas@yahoo.com>
2023-09-09 00:47:35 -0400

    update flakes and fix new session start dir bug

--- flake.nix
+++ flake.nix
@@ -3,25 +3,41 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        overlays.default = [
-          (final: prev: {
-            dmux = naersk-lib.buildPackage ./.;
-          })
-
-        ];
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+  outputs = {
+    naersk,
+    nixpkgs,
+    rust-overlay,
+    self,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = (import nixpkgs) {inherit system overlays;};
+      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      naersk' = pkgs.callPackage naersk {
+        cargo = toolchain;
+        rustc = toolchain;
+        clippy = toolchain;
+      };
+      buildInputs = with pkgs; lib.optionals stdenv.isDarwin [libiconv darwin.apple_sdk.frameworks.Security];
+    in {
+      defaultPackage = naersk'.buildPackage ./.;
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            tmux
+            fzf
+            cargo
+            rustc
+            rustfmt
+            pre-commit
+            rustPackages.clippy
+            rust-analyzer
+          ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
-      });
+    });
 }
